ce0f4d0fe433c970c6093f79c0e50eec727c0f11
oospina <oscarospina@worldoffice.com.co>
2025-06-04 09:24:21 -0500

    initialize Spring Boot application with Maven setup and basic configuration

---
+++ src/test/java/co/com/davivienda/KataDaviviendaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.davivienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataDaviviendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
